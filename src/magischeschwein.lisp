1976af75635429d98cb864b4a6bfc9851357b67e
ChuMutt <chufilthymutt@gmail.com>
2023-06-12 10:05:57 -0500

    use clingon

--- src/magischeschwein.lisp
+++ src/magischeschwein.lisp
@@ -2,11 +2,11 @@
 
 ;; Define your project functionality here...
 
-(defparameter *input-headers* (car (cl-csv:read-csv #P"../tests/headers.csv"))
+(defparameter *input-headers* (car (cl-csv:read-csv #P"/home/chu/.local/share/roswell/local-projects/chus/magischeschwein/tests/headers.csv"))
   "Contains the new (ledger-cli compatible) headers.")
 
 (defparameter *input-table*
-  (mapcar #'cdr (cddddr (cl-csv:read-csv #P"../tests/example-input-file.csv")))
+  (mapcar #'cdr (cddddr (cl-csv:read-csv #P"/home/chu/.local/share/roswell/local-projects/chus/magischeschwein/tests/example-input-file.csv")))
   "Contains the original table (the csv file to be inputted) in list form with
 the original header rows removed and the first column of values (transaction ids) removed as well.")
 
@@ -15,27 +15,49 @@ the original header rows removed and the first column of values (transaction ids
   "Contains the result of combining the new *input-headers* with *input-table*
 (the latter having its original headers removed).")
 
-(defun barf ()
+(defun barf-test ()
   (cl-csv:write-csv *output-table* :stream #P"../tests/example-output-file.csv"))
 
-(defun help ()
-  (format T "~&Usage:
+;; The naming with a "/" is just our convention.
+(defun cli/options ()
+  "Returns a list of options for our main command"
+  (list
+   (clingon:make-option
+    :flag
+    :description "short help."
+    :short-name #\h
+    :key :help)
+   (clingon:make-option
+    :string              ;; <--- string type: expects one parameter on the CLI.
+    :description "Name to greet"
+    :short-name #\n
+    :long-name "name"
+    :env-vars '("USER")     ;; <-- takes this default value if the env var exists.
+    :initial-value "lisper" ;; <-- default value if nothing else is set.
+    :key :name)))
 
-  magischeschwein [name]~&"))
+(defun cli/command ()
+  "A command to say hello to someone"
+  (clingon:make-command
+   :name "hello"
+   :description "say hello"
+   :version "0.0.0"
+   :authors '("John Doe <john.doe@example.org")
+   :license "BSD 2-Clause"
+   :options (cli/options) ;; <-- our options
+   :handler #'cli/handler))  ;; <--  to change. See below.
 
-(defun %main (argv)
-  "Parse CLI args."
-  (when (member "-h" argv :test #'equal)
-    ;; To properly parse command line arguments, use a third-party library such as
-    ;; clingon, unix-opts, defmain, adopt… when needed.
-    (help)
-    (uiop:quit))
-  (commence-with argv)
-  (uiop:quit))
+(defun cli/handler (cmd)
+  "The handler function of our top-level command"
+  (let ((free-args (clingon:command-arguments cmd))
+        (name (clingon:getopt cmd :name)))  ;; <-- using the option's :key
+    (format t "Hello, ~a!~%" name)
+    (format t "You have provided ~a more free arguments~%" (length free-args))
+    (format t "Bye!~%")))
 
 (defun main ()
   "Entry point for the executable.
   Reads command line arguments."
   ;; uiop:command-line-arguments returns a list of arguments (sans the script name).
   ;; We defer the work of parsing to %main because we call it also from the Roswell script.
-  (%main (uiop:command-line-arguments)))
+  (clingon:run (cli/command)))
