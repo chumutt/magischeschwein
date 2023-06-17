2821b20251f2f93646f394bab5b0163e1f75d264
ChuMutt <chufilthymutt@gmail.com>
2023-06-17 17:04:25 -0500

    refactor verbose top level handler

--- src/magischeschwein.lisp
+++ src/magischeschwein.lisp
@@ -25,19 +25,19 @@
     :initial-value NIL
     :key :output-file)))
 
+(defun top-level/verbose (&optional args verbose input-file output-file)
+  (when (> verbose 0)
+    (format T "The current verbosity level is set to ~A~%" verbose)
+    (format T "You have provided ~A arguments~%" (length args))
+    (format T "Input file: ~A!~%" input-file)
+    (format T "Output file location: ~A!~%" output-file)))
+
 (defun top-level/handler (cmd)
   (let ((args (clingon:command-arguments cmd))
         (input-file (clingon:getopt cmd :input-file))
         (output-file (clingon:getopt cmd :output-file))
         (verbose (clingon:getopt cmd :verbose)))
-    (when (> verbose 0)
-      (format T "The current verbosity level is set to ~A~%" verbose)
-      (format T "You have provided ~A arguments~%" (length args))
-      (format T "Input file: ~A!~%" input-file)
-      (format T "Output file location: ~A!~%" output-file))
-    (if (uiop:file-exists-p input-file)
-      (format T (uiop:merge-pathnames* input-file (uiop/os:getcwd)))
-      (format T "no file was inputted"))))
+    (top-level/verbose args verbose input-file output-file)))
 
 (defun top-level/command ()
   (clingon:make-command
@@ -46,35 +46,9 @@
     :version "0.0.1"
     :license "GNU GPL-3.0"
     :authors '("Chu the Pup <chufilthymutt@gmail.com>")
-    :usage "[-v] [-i <INPUT-FILE>] [-o <OUTPUT-LOCATION>]"
+    :usage "[-h] [-v] [-i <INPUT-FILE>] [-o <OUTPUT-LOCATION>]"
     :options (top-level/options)
     :handler #'top-level/handler))
-
-(defparameter *app* (top-level/command))
-
-(defun test-cmd-no-opts ()
-  (clingon:parse-command-line *app* nil))
-
-(defun test-cmd-verbose-w-no-opts ()
-  (clingon:parse-command-line *app* '("-v")))
-
-(defparameter *test-dir*
-  (asdf:system-relative-pathname "magischeschwein" "tests/"))
-
-(defparameter *test-input-file*
-  (uiop:native-namestring
-    (uiop:merge-pathnames* "example-input-file.csv" *test-dir*)))
-
-(defparameter *test-output-file*
-  (uiop:native-namestring
-    (uiop:merge-pathnames* "example-output-file.csv" *test-dir*)))
-
-(defun test-cmd-input-file-only ()
-  (clingon:parse-command-line *app* '("-i" *test-input-file*)))
-
-(defun test-whatis-input-file ()
-  (let ((c (test-cmd-input-file-only)))
-    (clingon:getopt c :input-file)))
 
 (defun main ()
   (let ((app (top-level/command)))
