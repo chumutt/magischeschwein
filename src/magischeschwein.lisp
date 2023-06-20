1102d03881d32c1dca26a641c1b70f07b96e25f3
ChuMutt <chufilthymutt@gmail.com>
2023-06-20 16:32:14 -0500

    process configuration

--- src/magischeschwein.lisp
+++ src/magischeschwein.lisp
@@ -1,63 +1,49 @@
 (in-package :magischeschwein)
 
-(defparameter *temporary-file* #P"/tmp/magischeschwein.tmp")
+(defparameter *ledger-cli-date-format*
+  '((:year 4) #\/ (:month 2) #\/ (:day 2)))
 
-(defparameter *date-format*
-  '((:month 2) #\/ (:day 2) #\/ (:year 4)))
+(defparameter *todays-date*
+  (local-time:format-timestring nil
+    (local-time:now)
+    :format *ledger-cli-date-format*))
 
-(defparameter *todays-date* (local-time:format-timestring nil (local-time:now) :format *date-format*))
-
-(defparameter *new-headers* '("date,payee,note,debit,credit,,code,"))
-
-(defparameter *ledger-cli-args* '("ledger"
-                                   "convert"
-                                   'NIL
-                                   "--input-date-format"
-                                   "\"%m/%d/%Y\" "
-                                   "--invert"
-                                   "--account"
-                                   "assets:checking"
-                                   "--rich-data"
-                                   "--now"
-                                   'NIL))
-
-(defun replace-at-index (lst index new-value)
-  (if (zerop index)
-    (cons new-value (cdr lst))
-    (cons (car lst)
-      (replace-at-index (cdr lst) (1- index) new-value))))
-
-(defun csvin-to-cli-args (lst csvin)
-  (replace-at-index lst 2 (uiop:native-namestring csvin)))
-
-(defun current-date-to-cli-args (lst datespec)
-  (replace-at-index lst 10 datespec))
-
-(defun with-cli-args (csvin)
-  (let ((lst1 (csvin-to-cli-args *ledger-cli-args* (uiop:native-namestring csvin))))
-    (let ((lst2 (current-date-to-cli-args lst1 *todays-date*)))
-      lst2)))
-
-(defun ledger-convert (csvin)
-  (uiop:run-program (with-cli-args csvin) :output T))
-
-(defun with-new-headers (lst)
-  (cons *new-headers* lst))
-
-(defun read-csv-string-from-file (file)
-  (cl-csv:read-csv (uiop:read-file-string file)))
-
-(defun strip-headers-and-1st-col (lst)
-  (mapcar #'cdr (cddddr lst)))
-
-(defun process-csv (file)
-  (with-new-headers (strip-headers-and-1st-col (read-csv-string-from-file file))))
+(defun test-launch ()
+   (uiop:launch-program
+     (list
+       "ledger"
+       "convert"
+       "/home/chu/.local/share/roswell/local-projects/chus/magischeschwein/tests/example-output-file.csv"
+       "--input-date-format"
+       "%m/%d/%Y"
+       "--invert"
+       "--account"
+       "assets:checking"
+       "--rich-data"
+       "--file"
+       "/home/chu/.local/share/roswell/local-projects/chus/magischeschwein/tests/example-journal.dat"
+       "--now=2023/06/20")
+     :output *standard-output*))
 
 (defun top-level/handler (cmd)
-  (let ((input-file (clingon:getopt cmd :input-file)))
-    (prog2
-      (cl-csv:write-csv (process-csv input-file) :stream *temporary-file*)
-      (ledger-convert *temporary-file*))))
+  (let ((input-file   (clingon:getopt cmd :input-file))
+        (journal-file (clingon:getopt cmd :journal-file))
+        (account-name (clingon:getopt cmd :account-name))
+        (verbose      (clingon:getopt cmd :verbose)))
+    (uiop:launch-program
+      (list
+        "ledger"
+        "convert"
+        (format nil "~a" input-file)
+        "--input-date-format"
+        "%m/%d/%Y"
+        "--invert"
+        "--account"
+        (format nil "~a" account-name)
+        "--rich-data"
+        "--file"
+        (format nil "~a" journal-file)
+        (format nil "--now=~a" *todays-date*)))))
 
 (defun top-level/options ()
   (list
@@ -66,7 +52,26 @@
     :description "the input csv file you want to convert"
     :short-name #\i
     :long-name "input-file"
-    :key :input-file)))
+    :key :input-file)
+   (clingon:make-option
+     :filepath
+     :description "your pre-existing ledger journal file"
+     :short-name #\j
+     :long-name "journal-file"
+     :key :journal-file)
+   (clingon:make-option
+     :string
+     :description "name of primary checking account in ledger"
+     :short-name #\a
+     :long-name "account-name"
+     :initial-value "assets:checking"
+     :key :account-name)
+   (clingon:make-option
+     :counter
+     :description "verbosity level"
+     :short-name #\v
+     :long-name "verbose"
+     :key :verbose)))
 
 (defun top-level/command ()
   (clingon:make-command
@@ -75,7 +80,7 @@
     :version "0.0.1"
     :license "GNU GPL-3.0"
     :authors '("Chu the Pup <chufilthymutt@gmail.com>")
-    :usage "[-h] [-i <INPUT-FILE>]"
+    :usage "FIXME"
     :options (top-level/options)
     :handler #'top-level/handler))
 
